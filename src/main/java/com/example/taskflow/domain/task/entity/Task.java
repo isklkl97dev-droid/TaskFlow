1ba9ad4be041351b6eaf04611d388ef3b9aba8e6
LENOVO <isklkl97.dev@3vi.co.kr>
2025-10-15 11:27:46 +0900

    feat: Task 엔티티 및 레파지토리 기본 세팅

--- src/main/java/com/example/taskflow/domain/task/entity/Task.java
+++ src/main/java/com/example/taskflow/domain/task/entity/Task.java
@@ -1,4 +1,41 @@
 package com.example.taskflow.domain.task.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity()
+@Getter
+@NoArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private LocalDate startDate;
+
+    private LocalDate dueDate;
+
+    public Task(
+            String name,
+            String title,
+            String content,
+            LocalDate startDate,
+            LocalDate dueDate
+            ) {
+        this.name = name;
+        this.title = title;
+        this.content = content;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+    }
 }
--- src/main/java/com/example/taskflow/domain/task/repository/TaskRepository.java
+++ src/main/java/com/example/taskflow/domain/task/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.example.taskflow.domain.task.repository;
 
-public class TaskRepository {
+import com.example.taskflow.domain.task.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
