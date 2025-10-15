d8bd5457c292c31dbd204edecd3b66d5144db901
LENOVO <isklkl97.dev@3vi.co.kr>
2025-10-15 10:55:33 +0900

    feat: 유저엔티티 및 레파지토리 생성

--- src/main/java/com/example/taskflow/domain/user/entity/User.java
+++ src/main/java/com/example/taskflow/domain/user/entity/User.java
@@ -1,4 +1,28 @@
 package com.example.taskflow.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
